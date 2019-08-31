cc1cfc072aa44e0e03e2d8d229d557c84ec5650e
Yonatan Bentzur <ybentzur7@gmail.com>
2019-08-30 21:50:24 -0700

    Project configs, meta data updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SMPL Coding Club`,
+    description: `Start your journey in the wonderful world of programming`,
+    author: `Yonatan Bentzur`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `SMPL Coding Club`,
+        short_name: `Coding Club`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#FFFFFF`,
+        theme_color: `#6100FF`,
+        display: `standalone`,
+        icon: `src/images/SMPL-logo-text-white.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
