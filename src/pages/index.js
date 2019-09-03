3738f4a15bbaa45a2b66e18c00f645cf1eef62e7
Yonatan Bentzur <ybentzur7@gmail.com>
2019-09-02 17:59:32 -0700

    Updated home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,45 @@
-import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+import React from 'react'
 
 import HeroHeader from '../components/HeroHeader'
-import Image from '../components/image'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import ResourceCardList from '../components/ResourceCardList'
 
 const IndexPage = () => (
   <>
     <HeroHeader />
     <Layout noHeader={true}>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <Section>
+        <h2>Welcome to the SMPL coding club!</h2>
+        <p>
+          Here are a few links to the most popular programming languages for
+          beginners. Click on any of them to learn more.
+        </p>
+        <p>Now go learn, have fun and build something great.</p>
+      </Section>
+      <Section>
+        <ResourceCardList filter={popularResourceFilter} />
+      </Section>
+      <Section>
+        <p>
+          To browse the list of all available programming languages and
+          resources click <Link to="/resources">here</Link>.
+        </p>
+      </Section>
     </Layout>
   </>
 )
+
+const popularResources = ['scratch', 'python', 'javascript']
+const popularResourceFilter = resource => {
+  return popularResources.includes(resource.id)
+}
+
+const Section = styled.div`
+  margin: 2rem 0;
+`
 
 export default IndexPage
