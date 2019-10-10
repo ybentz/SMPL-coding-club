f667f5b3e9f91e1f462c2445377bcc3854250e62
Yonatan Bentzur <ybentzur7@gmail.com>
2019-10-09 22:15:26 -0700

    Added sitemap + robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,28 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+// Setup for robots.txt plugin - disallow all on Netlify deploy previews
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = `${process.env.HOSTNAME}`,
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 const config = {
   siteMetadata: {
     title: `SMPL Coding Club`,
     description: `Start your journey in the wonderful world of programming`,
     author: `Yonatan Bentzur`,
-    siteUrl: `${process.env.HOSTNAME}`,
+    siteUrl,
     logo: `/images/SMPL-logo-text.png`,
     twitterUsername: '@ybentzur',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-source-filesystem`,
@@ -46,6 +57,27 @@ const config = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
   ],
 }
 
